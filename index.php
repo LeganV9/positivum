a8f61449ffae6ff223151225369c6b4c00ccf685
LeganV9 <yanisrenaud77@gmail.com>
2022-06-13 23:58:48 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php include 'header.php' ?>
+
+        <h2 class="title">What is your current mood?</h2> <!-- Titre et question principale sur le site --> 
+
+        <div class="wrapper-emoji">
+            <div onclick=window.location.href='https://google.com' class="emoji">🖕</div>
+            <div onclick=window.location.href='https://google.com' class="emoji">😩</div>
+            <div onclick=window.location.href='https://google.com' class="emoji">😐</div>
+            <div onclick=window.location.href='https://google.com' class="emoji">😋</div>
+            <div onclick=window.location.href='https://google.com' class="emoji">🏆</div>
+        </div>
+
+        <p class="bodytext">Hi, my name is Yanis.
+        Here you can choose your mood each day and directly get a quote allowing you to improve your state of mind and confidence in order to have a successful day!
+        This project is mainly intended for new entrepreneurs who do not have the support of those around them yet!</p> <!-- Texte utilisé pour présenter le projet --> 
+
+    </body>
+</html>
