ab414506ba17293c8ca798bf03923eb00b0bd35b
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2022-06-16 21:45:24 +0000

    Reset home

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div class="header">
-    <a href="/positivum" class="button">TEST</a>
+    <a href="/positivum" class="button">Home</a>
     <a href="/positivum/blog.php" class="button">Blog</a>
     <a href="/positivum/about.php" class="button">About</a>
     <a href="/positivum/contact.php" class="button">Contact</a>
